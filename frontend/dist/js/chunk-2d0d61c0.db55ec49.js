914362a275ca45804cba4e22186609e6dc81887f
avkuznetsov54 <avkuznetsov54@gmail.com>
2020-02-03 01:59:21 +0700

    #1

---
+++ frontend/dist/js/chunk-2d0d61c0.db55ec49.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d61c0"],{"70c4":function(t,n,e){"use strict";e.r(n);var c=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an Contacts page")])])}],a=e("2877"),u={},i=Object(a["a"])(u,c,s,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0d61c0.db55ec49.js.map
---
+++ frontend/dist/js/chunk-2d22db6b.17f97a9c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22db6b"],{f957:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an Help page")])])}],c=t("2877"),l={},u=Object(c["a"])(l,s,a,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d22db6b.17f97a9c.js.map
